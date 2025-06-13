a87898ba19fd93acc86efaec2755a0620331d2a1
LiongJiaHui <liongjh2004@1utar.my>
2025-06-13 14:47:18 +0800

    - done the html part for the Main Page.blade.php - setup the student section and administrator section - modify the route for web.php - add the footer into the MainPage.blade.php

---
+++ resources/views/MainPage.blade.php
@@ -0,0 +1,29 @@
+<DOCTYPE! html> 
+<html>
+
+<head>
+    <title>Main Page</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body>
+    <div>
+        <div> 
+            <h1>Course and University Recommendation System</h1>
+        </div>
+        <div>
+            <h2>Please choose your role: </h2>
+        </div>
+        <div>
+            <div>
+                <button>Student</button>
+            </div>
+            <div>
+                <button>Administrator</button>
+            </div>
+        </div>
+    </div>
+    <x-footer />
+</body>
+
+</html>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div>
+        <p>&copy; 2025 Course and University recommentation system. All right reserved. </p>
+    </div>
+</footer>
