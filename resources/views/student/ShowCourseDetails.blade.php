08ff8d342391edb0eff1e460fd987a29d8b42276
LiongJiaHui <liongjh2004@1utar.my>
2025-07-30 00:07:56 +0800

    - add on the show course details for the student section

---
+++ resources/views/student/ShowCourseDetails.blade.php
@@ -0,0 +1,20 @@
+<head>
+    <title>Course and University Recommendation System: Course and University Details from recommended list</title>
+
+</head>
+
+<body>
+    <x-header title="Course and University Details from recommended list" />
+    <div>
+        <div>
+
+        </div>
+
+        <div>
+            <a href="">
+                <button>Back</button>
+            </a>
+        </div>
+    </div>
+    <x-footer />
+</body>
